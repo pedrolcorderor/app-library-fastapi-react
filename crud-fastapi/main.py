996c74b232e265eb67667bf3f0149e29bc7907d7
Pedro cordero <pedrolcorderor@gmail.com>
2024-07-12 16:45:36 -0400

    cambios

--- crud-fastapi/main.py
+++ crud-fastapi/main.py
@@ -6,8 +6,17 @@ import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from Models.books import Book, BookId
+from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
+
+
+@app.get("/scalar", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title= app.title + "- Scalar",
+    )
 
 models.Base.metadata.create_all(bind= engine)
 origins = [
